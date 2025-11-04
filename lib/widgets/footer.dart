38bebcc03e929c8b5de472f9d110101f9a682122
M Zidan Kusuma Firdaus <zidankusumafirdaus.school@gmail.com>
2025-11-04 13:48:17 +0700

    feat: add contact & home route

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -14,16 +14,30 @@ class CustomFooter extends StatelessWidget {
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       currentIndex: currentIndex,
-      onTap: onTap,
+      onTap: (index) {
+        onTap(index); // tetap panggil callback parent
+
+        switch (index) {
+          case 0:
+            Navigator.pushNamed(context, '/home');
+            break;
+          case 1:
+            Navigator.pushNamed(context, '/goal');
+            break;
+          case 2:
+            Navigator.pushNamed(context, '/info');
+            break;
+          case 3:
+            Navigator.pushNamed(context, '/contact');
+            break;
+        }
+      },
       showSelectedLabels: false,
       showUnselectedLabels: false,
       selectedItemColor: Colors.orange,
       unselectedItemColor: Colors.black,
       items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home_outlined),
-          label: 'Home',
-        ),
+        BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home'),
         BottomNavigationBarItem(
           icon: Icon(Icons.track_changes_outlined),
           label: 'Goal',
